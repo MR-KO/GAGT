f1ec806b68dcf6e64aebcbfc746fdf2a40f27f3a
DrHaXX <c0nd3mn3d@hotmail.com>
2014-02-05 22:39:58 +0100

    Attempt at octants

--- Assignment1/framework1/basic_midpoint/init.c
+++ Assignment1/framework1/basic_midpoint/init.c
@@ -58,8 +58,8 @@ void PutPixel(SDL_Surface *surface, int x, int y, Uint32 pixel) {
 
 
 void DrawFigure(SDL_Surface *screen) {
-	// int i, mid_x = screen->w / 2, mid_y = screen->h / 2, size = 100;
-	// Uint32 colour;
+	int mid_x = screen->w / 2, mid_y = screen->h / 2, size = 100;
+	Uint32 colour;
 
 	if (SDL_MUSTLOCK(screen)) {
 		if (SDL_LockSurface(screen)) {
@@ -67,30 +67,30 @@ void DrawFigure(SDL_Surface *screen) {
 		}
 	}
 
-	// for (i = 0; i < 32; i++) {
-	// 	/* Set drawing colour */
-	// 	switch((i >> 2) % 4) {
-	// 		case 0:
-	// 			colour = SDL_MapRGB(screen->format, 0xFF, 0, 0);
-	// 			break;
-	// 		case 1:
-	// 			colour = SDL_MapRGB(screen->format, 0, 0xFF, 0);
-	// 			break;
-	// 		case 2:
-	// 			colour = SDL_MapRGB(screen->format, 0, 0, 0xFF);
-	// 			break;
-	// 		case 3:
-	// 			colour = SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF);
-	// 			break;
-	// 	}
+	for (int i = 0; i < 32; i++) {
+		/* Set drawing colour */
+		switch((i >> 2) % 4) {
+			case 0:
+				colour = SDL_MapRGB(screen->format, 0xFF, 0, 0);
+				break;
+			case 1:
+				colour = SDL_MapRGB(screen->format, 0, 0xFF, 0);
+				break;
+			case 2:
+				colour = SDL_MapRGB(screen->format, 0, 0, 0xFF);
+				break;
+			case 3:
+				colour = SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF);
+				break;
+		}
 
-	// 	/* draw a line */
-	// 	mla(screen, mid_x, mid_y,
-	// 		(int) roundf(size * cos(2 * M_PI / 32 * i)) + mid_x,
-	// 		(int) roundf(size * sin(2 * M_PI / 32 * i)) + mid_y, colour);
-	// }
+		/* draw a line */
+		mla(screen, mid_x, mid_y,
+			(int) roundf(size * cos(2 * M_PI / 32 * i)) + mid_x,
+			(int) roundf(size * sin(2 * M_PI / 32 * i)) + mid_y, colour);
+	}
 
-	mla(screen, 100, 100, 200, 90, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
+	// mla(screen, 100, 100, 200, 90, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
 
 	/* It would be more efficient to call SDL_UpdateRect(), but I do not really
 	 * care.
--- Assignment1/framework1/basic_midpoint/mla.c
+++ Assignment1/framework1/basic_midpoint/mla.c
@@ -35,6 +35,54 @@ double f(double x, double y, double x0, double y0, double x1, double y1) {
 	return (y0 - y1) * x + (x1 - x0) * y + x0 * y1 - x1 * y0;
 }
 
+void determine_octant(double x0, double x1, double y0, double y1) {
+	double x = x1 - x0;
+	double y = y1 - y0;
+	double xy = x - y;
+
+	printf("x0 = %g, y0 = %g, x1 = %g, y1 = %g:  ", x0, y0, x1, y1);
+	printf("\tx1 - x0 = %g, y1 - y0 = %g, (x1 - x0) - (y1 - y0) = %g   ",
+		x, y, xy);
+
+	if (x >= 0 && y < 0 && xy >= 0) {
+		printf("\tFirst octant (0 < m <= 1)");
+	}
+
+	if (x >= 0 && y < 0 && xy < 0) {
+		printf("\tFifth octant (?)");
+	}
+
+
+	if (x >= 0 && y >= 0 && xy >= 0) {
+		printf("\tSeventh octant (0 < m <= -1)");
+	}
+
+	if (x >= 0 && y >= 0 && xy < 0) {
+		printf("\tSixth octant (-1 < m <= Inf)");
+	}
+
+
+
+	if (x < 0 && y < 0 && xy >= 0) {
+		printf("\tFourth octant (?)");
+	}
+
+	if (x < 0 && y < 0 && xy < 0) {
+		printf("\tThird octant (?)");
+	}
+
+
+	if (x < 0 && y >= 0 && xy >= 0) {
+		printf("Second octant (?)");
+	}
+
+	if (x < 0 && y >= 0 && xy < 0) {
+		printf("Eight octant (?)");
+	}
+
+	printf("\n");
+}
+
 /*
  * Midpoint Line Algorithm
  *
@@ -70,32 +118,30 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
 		iy = -1;
 	}
 
-	int y = y0;
+	determine_octant(x0, x1, y0, y1);
 
+	int y = y0;
 	double d = f(x0 + 1, y0 + 0.5, x0, y0, x1, y1);
-    printf("d= %g\n", d);
+    // printf("d = %g\n", d);
 
 	for(int x = x0; x != x1; x += ix) {
 		PutPixel(s, x, y, colour);
-		double rv = f(x + 1, y + 0.5, x0, y0, x1, y1);
+		// double rv = f(x + 1, y + 0.5, x0, y0, x1, y1);
 
-		printf("x = %d, y = %d, f = %g, d = %g\n", x, y, rv, d);
+		// printf("x = %d, y = %d, f = %g, d = %g\n", x, y, rv, d);
 
 		if (iy == 1) {
             if (d < 0) {
                 y += iy;
                 d = d + ((x1 - x0) + (y0 - y1));
-            }
-            else if (d >= 0) {
+            } else if (d >= 0) {
                 d = d + (y0 - y1);
             }
-        }
-        else if (iy == -1) {
+        } else if (iy == -1) {
             if (d >= 0) {
                 y += iy;
                 d = d + (-(x1 - x0) + (y0 - y1));
-            }
-            else if (d < 0) {
+            } else if (d < 0) {
                 d = d + (y0 - y1);
             }
         }
