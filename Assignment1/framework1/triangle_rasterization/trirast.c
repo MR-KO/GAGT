d14a591f6a496121e710a0b0c64c594f14f89cc9
Timo Dobber <timo.dobber@student.uva.nl>
2014-02-06 22:58:40 +0100

    first triangles drawn

--- Assignment1/framework1/triangle_rasterization/trirast.c
+++ Assignment1/framework1/triangle_rasterization/trirast.c
@@ -18,6 +18,7 @@
 #include <math.h>
 
 #include "types.h"
+#include "trirast.h"
 
 /*
  * Rasterize a single triangle.
@@ -26,14 +27,83 @@
  * The triangle is drawn in color (r,g,b).
  */
 
-void
-draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
-    byte r, byte g, byte b)
-{
+inline double f_01(int x, int y, double x0, double y0, double x1, double y1) {
+    return (y0 - y1) * x + (x1 - x0) * y + x0 * y1 - x1 * y0;
 }
 
-void
-draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, float y2,
-    byte r, byte g, byte b)
-{
+inline double f_12(int x, int y, double x1, double y1, double x2, double y2) {
+    return (y1 - y1) * x + (x2 - x1) * y + x1 * y2 - x2 * y1;
+}
+
+inline double f_20(int x, int y, double x0, double y0, double x2, double y2) {
+    return (y2 - y0) * x + (x0 - x2) * y + x2 * y0 - x0 * y2;
+}
+
+void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
+    byte r, byte g, byte b) {
+
+	int xmin, xmax, ymin, ymax;
+	float alpha, beta, gamma;
+
+	xmin = getMin(x0, x1, x2);
+	ymin = getMin(y0, y1, y2);
+	xmax = getMax(x0, x1, x2);
+	ymax = getMax(y0, y1, y2);
+
+	for (int y = ymin; y <= ymax; y++) {
+		for (int x = xmin; x <= xmax; x++) {
+			beta = f_20(x, y, x0, y0, x2, y2) / f_20(x1, y1, x0, y0, x2, y2);
+			gamma = f_01(x, y, x0, y0, x1, y1) / f_01(x2, y2, x0, y0, x1, y1);
+			alpha = 1 - beta - gamma;
+			// printf("alpha: %g, beta: %g, gamma: %g\n", alpha, beta, gamma);
+			if (alpha > 0 && beta > 0 && gamma > 0) {
+				PutPixel(x, y, r, g, b);
+			}
+		}
+	}
+
+}
+
+void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, float y2,
+    byte r, byte g, byte b) {
+
+
+}
+
+int getMin(int a, int b, int c) {
+	if (a < b) {
+		if (a < c) {
+			return a;
+		}
+		else {
+			return c;
+		}
+	}
+	else {
+		if (b < c) {
+			return b;
+		}
+		else {
+			return c;
+		}
+	}
+}
+
+int getMax(int a, int b, int c) {
+	if (a > b) {
+		if (a > c) {
+			return a;
+		}
+		else {
+			return c;
+		}
+	}
+	else {
+		if (b > c) {
+			return b;
+		}
+		else {
+			return c;
+		}
+	}
 }
--- Assignment1/framework1/triangle_rasterization/trirast.h
+++ Assignment1/framework1/triangle_rasterization/trirast.h
@@ -8,5 +8,8 @@ void    draw_triangle(float x0, float y0, float x1, float y1,
 void    draw_triangle_optimized(float x0, float y0, float x1, float y1,
             float x2, float y2, byte r, byte g, byte b);
 
+int getMin(int a, int b, int c);
+int getMax(int a, int b, int c);
+
 #endif
 
