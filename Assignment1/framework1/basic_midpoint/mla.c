1eabc8c615e48fa595ee5f87a0dfd991289406ce
Timo Dobber <timo.dobber@student.uva.nl>
2014-02-07 12:15:56 +0100

    comments and better style

--- Assignment1/framework1/basic_midpoint/mla.c
+++ Assignment1/framework1/basic_midpoint/mla.c
@@ -5,10 +5,10 @@
  * Created by ...... Jurgen Sturm
  *
  * Student name .... Kevin Ouwehand & Timo Dobber
- * Student email ...  & timo.dobber@gmail.com
- * Collegekaart .... & 10386726
- * Date ............ 7/2/2014
- * Comments ........
+ * Student email ... c0nd3mn3d@hotmail.com & timo.dobber@gmail.com
+ * Collegekaart .... 10420908 & 10386726
+ * Date ............ 07/02/2014
+ * Comments ........ NULL
  *
  *
  * (always fill in these fields before submitting!!)
@@ -18,10 +18,15 @@
 #include "init.h"
 #include "mla.h"
 
+/* Define the function used to calculate d. */
 double f(double x, double y, double x0, double y0, double x1, double y1) {
     return (y0 - y1) * x + (x1 - x0) * y + x0 * y1 - x1 * y0;
 }
 
+/* 
+ * Determines in which octant the line will fall, first octant is 0-45 degrees,
+ * second octant is 45-90 degrees and so on.
+ */
 int determine_octant(double x0, double x1, double y0, double y1) {
     double x = x1 - x0;
     double y = y1 - y0;
@@ -108,6 +113,7 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
         return;
     }
 
+    /* Swap x1 and x0 when x1 is smaller than x0. */
     if (x1 > x0) {
         ix = 1;
     } else {
@@ -126,46 +132,57 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
         iy = -1;
     }
 
+    /* 
+     *  Checks whether the line is steep, because then the x and y need to
+     *  be swapped. 
+     */
     if (octant == 2 || octant == 3 || octant == 6 || octant == 7) {
-        *to_increment = x0;
+        (*to_increment) = x0;
         double d = f(x0 + 1, y0 + 0.5, x0, y0, x1, y1);
 
         for (int y = y0; y != y1; y += iy) {
             PutPixel(s, *to_increment, y, colour);
 
-            if (ix == 1) {
-                if (octant == 2 || octant == 6) {
-                    d = checkConditions(d, x0, x1, y1, y0, x0, x1, -1, ix, to_increment);
-                } else {
-                    d = checkConditions(d, x0, x1, y1, y0, x0, x1, 1, ix, to_increment);
-                }
+            /* 
+             *  Octant 2 and 6 need to be seperated since they both have an 
+             *  exact vertical line, in opposite directions.
+             */
+            if (octant == 2 || octant == 6) {
+                d = check_conditions(d, x0, x1, y1, y0, x0, x1, -1, ix,
+                    to_increment);
+            } else {
+                d = check_conditions(d, x0, x1, y1, y0, x0, x1, 1, ix,
+                    to_increment);
             }
         }
     } else {
-        *to_increment = y0;
+        (*to_increment) = y0;
         double d = f(x0 + 1, y0 + 0.5, x0, y0, x1, y1);
 
         for (int x = x0; x != x1; x += ix) {
             PutPixel(s, x, *to_increment, colour);
-            d = checkConditions(d, x0, x1, x1, x0, y0, y1, iy, iy, to_increment);
+            d = check_conditions(d, x0, x1, x1, x0, y0, y1, iy, iy, to_increment);
         }
     }
 
     return;
 }
 
-double checkConditions(double d, int x0, int x1, int left_1, int left_2, int right_1, int right_2,
-    int sign, int increment, int* to_increment) {
+/*
+ * Checks all the conditions and calculates the new d incremental.
+ */
+double check_conditions(double d, int x0, int x1, int left_1, int left_2,
+    int right_1, int right_2, int sign, int increment_value, int* to_increment) {
 
-    if ((increment == 1 && d < 0) || (increment == -1 && d >= 0)) {
+    if ((increment_value == 1 && d < 0) || (increment_value == -1 && d >= 0)) {
         if (x1 - x0 == 0) {
             d = d + (right_1 - right_2);
         }
         else {
-            (*to_increment) += increment;
+            (*to_increment) += increment_value;
             d = d + (sign * (left_1 - left_2) + (right_1 - right_2));
         }
-    } else if ((increment == 1 && d >= 0) || (increment == -1 && d < 0)) {
+    } else if ((increment_value == 1 && d >= 0) || (increment_value == -1 && d < 0)) {
         d = d + (right_1 - right_2);
     }
 
--- Assignment1/framework1/basic_midpoint/mla.h
+++ Assignment1/framework1/basic_midpoint/mla.h
@@ -11,7 +11,7 @@
 
 int determine_octant(double x0, double y0, double x1, double y1);
 void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour);
-double checkConditions(double d, int x0, int x1, int left_1, int left_2, int right_1, int right_2,
-	int sign, int increment, int* to_increment);
+double check_conditions(double d, int x0, int x1, int left_1, int left_2, int right_1, int right_2,
+	int sign, int increment_value, int* to_increment);
 
 #endif /* JS_MLA_H */
