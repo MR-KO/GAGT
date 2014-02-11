8541806a8502c777b1a2ae3e182e690970d756da
DrHaXX <c0nd3mn3d@hotmail.com>
2014-02-11 15:03:03 +0100

    YES, IT FUCKING WORKS

--- Assignment2/framework2/positioning_the_camera/lookat.c
+++ Assignment2/framework2/positioning_the_camera/lookat.c
@@ -31,13 +31,14 @@ void myLookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ,
               GLdouble upX, GLdouble upY, GLdouble upZ) {
 
 	// Translate by the vector -VRP
-	glTranslatef(-eyeX, -eyeY, -eyeZ);
+	//glTranslatef(-eyeX, -eyeY, -eyeZ);
 
 	// Change the coordinate system to camera coordinates.
 	// For that, we need the three vectors that represent the camera coordinates
 	// in world coordinates
 	// First the cz vector (pointing from Pcamera to Plookat)
 	GLdouble cz[3];
+
 	cz[0] = eyeX - centerX;
 	cz[1] = eyeY - centerY;
 	cz[2] = eyeZ - centerZ;
@@ -67,9 +68,9 @@ void myLookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ,
 	// Get cy, which is orthogonal to cx and cz
 	GLdouble cy[3];
 
-	cy[0] = cx[1] * cz[2] - cx[2] * cz[1];
-	cy[1] = cx[2] * cz[0] - cx[0] * cz[2];
-	cy[2] = cx[0] * cz[1] - cx[1] * cz[0];
+	cy[0] = cz[1] * cx[2] - cz[2] * cx[1];
+	cy[1] = cz[2] * cx[0] - cz[0] * cx[2];
+	cy[2] = cz[0] * cx[1] - cz[1] * cx[0];
 
 	// Normalize cy
 	length = sqrt(cy[0] * cy[0] + cy[1] * cy[1] + cy[2] * cy[2]);
@@ -80,9 +81,9 @@ void myLookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ,
 
 	// Get the rotation matrix R
 	GLfloat R[16] = {
-		cx[0], cy[0], -cz[0], 0,
-		cx[1], cy[1], -cz[1], 0,
-		cx[2], cy[2], -cz[2], 0,
+		cx[0], cy[0], cz[0], 0,
+		cx[1], cy[1], cz[1], 0,
+		cx[2], cy[2], cz[2], 0,
 		0, 0, 0, 1
 	};
 
@@ -94,7 +95,14 @@ void myLookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ,
 		0, 0, 0, 1
 	};
 
-	glMultMatrixf(RT);
-	glMultMatrixf(R);
+	// Get the translate matrix E
+	GLfloat E[16] = {
+		1, 0, 0, eyeX,
+		0, 1, 0, eyeY,
+		0, 0, 1, eyeZ,
+		0, 0, 0, 1
+	};
 
+	glMultMatrixf(R);
+	// glMultMatrixf(R);
 }
