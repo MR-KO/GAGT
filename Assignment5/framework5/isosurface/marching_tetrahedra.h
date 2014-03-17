14375054565809ca36ffe3ed0f9bff33fe37c460
DrHaXX <c0nd3mn3d@hotmail.com>
2014-03-17 16:13:27 +0100

    Shit opgefuckt 2.0

--- Assignment5/framework5/isosurface/marching_tetrahedra.h
+++ Assignment5/framework5/isosurface/marching_tetrahedra.h
@@ -4,12 +4,10 @@
 #include "v3math.h"
 #include "volume.h"
 
-typedef struct
-{
+typedef struct {
     vec3    p[3];
     vec3    n[3];
-}
-triangle;
+} triangle;
 
 int generate_cell_triangles(triangle *triangles, cell c, unsigned char isovalue);
 
