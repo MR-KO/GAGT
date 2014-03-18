97e380e372fe81c91ad4ceee793d364394d61125
DrHaXX <c0nd3mn3d@hotmail.com>
2014-03-18 13:57:50 +0100

    Added finish detection for the ball to the game

--- Assignment6/framework6/main.cpp
+++ Assignment6/framework6/main.cpp
@@ -33,55 +33,80 @@ b2World* world;
 b2Body* body;
 int current_level;
 
+float ball_radius = 0.1F;
+float finish_width = 0.05F;
+float finish_height = 0.05F;
+
 /*
  * Load a given world, i.e. read the world from the `levels' data structure and
  * convert it into a Box2D world.
  */
 void load_world(unsigned int level) {
 
-    if (level >= num_levels)
-    {
-        // Note that level is unsigned but we still use %d so -1 is shown as
-        // such.
-        printf("Warning: level %d does not exist.\n", level);
-        return;
-    }
+	if (level >= num_levels) {
+		// Note that level is unsigned but we still use %d so -1 is shown as
+		// such.
+		printf("Warning: level %d does not exist.\n", level);
+		return;
+	}
 
-    // Create a Box2D world and populate it with all bodies for this level
-    // (including the ball).
-    current_level = level + 1;
-    b2Vec2 gravity (0, -9.81);
-    bool do_sleep = true;
+	// Create a Box2D world and populate it with all bodies for this level
+	// (including the ball).
+	current_level = level + 1;
+	b2Vec2 gravity (0, -9.81);
+	bool do_sleep = true;
 
-    world = new b2World(gravity);
-    world->SetAllowSleeping(do_sleep);
+	world = new b2World(gravity);
+	world->SetAllowSleeping(do_sleep);
 
-    // printf("num_polygons = %i\n", levels[0].num_polygons);
-    // printf("is_Dynamic = %d\n", levels[0].polygons[0].is_dynamic);
+	// printf("num_polygons = %i\n", levels[0].num_polygons);
+	// printf("is_Dynamic = %d\n", levels[0].polygons[0].is_dynamic);
 
-    // Setup ball.
-    b2BodyDef body_def;
-    body_def.type = b2_dynamicBody;
-    body_def.position.Set(levels[level].start.x, levels[level].start.y);
-    body = world->CreateBody(&body_def);
+	// Setup ball.
+	b2BodyDef body_def;
+	body_def.type = b2_dynamicBody;
+	body_def.position.Set(levels[level].start.x, levels[level].start.y);
+	body = world->CreateBody(&body_def);
 
-    b2CircleShape circle;
-    // circle.m_p.Set(3.0f, 4.0f);
-    circle.m_radius = 0.5f;
+	b2CircleShape circle;
+	// circle.m_p.Set(3.0f, 4.0f);
+	circle.m_radius = ball_radius;
 
-    b2FixtureDef fixture_def;
-    fixture_def.shape = &circle;
-    fixture_def.density = 1.0f;
-    fixture_def.friction = 0.3f;
+	b2FixtureDef fixture_def;
+	fixture_def.shape = &circle;
+	fixture_def.density = 1.0f;
+	fixture_def.friction = 0.3f;
 
-    body->CreateFixture(&fixture_def);
-    b2Vec2 triangle[3];
+	body->CreateFixture(&fixture_def);
+	b2Vec2 triangle[3];
 
-    // Setup rest of level.
-    // for (int i = 0; i < levels[level].num_polygons; i++) {
-    //     [levels[level].polygons[i].num_verts];
-    //     triangle
-    // }
+	// Setup rest of level.
+	// for (int i = 0; i < levels[level].num_polygons; i++) {
+	//     [levels[level].polygons[i].num_verts];
+	//     triangle
+	// }
+}
+
+float calcDistance(float x1, float y1, float x2, float y2) {
+	return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+/* Returns true if the ball is on the edge of the ball, or inside the ball. */
+bool ballHasReachedFinish(b2Vec2 ball_position) {
+	float finish_x = levels[current_level].end.x;
+	float finish_y = levels[current_level].end.y;
+	float ball_x = ball_position.x;
+	float ball_y = ball_position.y;
+
+	/* Calculate the distances between the 4 corners of the finish square and the center of the ball. */
+	float distance1 = calcDistance(finish_x, finish_y, ball_x, ball_y);
+	float distance2 = calcDistance(finish_x, finish_y, ball_x, ball_y);
+	float distance3 = calcDistance(finish_x, finish_y, ball_x, ball_y);
+	float distance4 = calcDistance(finish_x, finish_y, ball_x, ball_y);
+
+	/* If one of them is lower than the radius of the ball, the ball has reached the finish. */
+	return ((distance1 <= ball_radius) || (distance2 <= ball_radius) ||
+		(distance3 <= ball_radius) || (distance4 <= ball_radius));
 }
 
 
@@ -89,159 +114,164 @@ void load_world(unsigned int level) {
  * Called when we should redraw the scene (i.e. every frame).
  * It will show the current framerate in the window title.
  */
-void draw(void)
-{
-    int time = glutGet(GLUT_ELAPSED_TIME);
-    int frametime = time - last_time;
-    frame_count++;
+void draw(void) {
+	int time = glutGet(GLUT_ELAPSED_TIME);
+	int frametime = time - last_time;
+	frame_count++;
 
-    // Clear the buffer
-    glColor3f(0, 0, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
+	// Clear the buffer
+	glColor3f(0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
 
 
-    //
-    // Do any logic and drawing here.
-    //
+	//
+	// Do any logic and drawing here.
+	//
 
-    // suggested vel = 8, suggested pos = 3.
-    float32 timestep = 1.0f / 60.0f;
-    int32 velocity_iterations = 6;
-    int32 position_iterations = 2;
-    if (play) {
-        world->Step(timestep, velocity_iterations, position_iterations);
-    }
+	// suggested vel = 8, suggested pos = 3.
+	float32 timestep = 1.0f / 60.0f;
+	int32 velocity_iterations = 6;
+	int32 position_iterations = 2;
 
-    b2Vec2 position = body->GetPosition();
+	b2Vec2 position = body->GetPosition();
 
-    // Draw circle
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(position.x, position.y);
-    for (int i = 0; i <= 360; i += 5) {
-        glVertex2f(position.x + sin(i) * 0.1, position.y + cos(i) * 0.1);
-    }
-    glEnd();
+	if (play) {
+		world->Step(timestep, velocity_iterations, position_iterations);
+		if (ballHasReachedFinish(position)) {
+			fprintf(stderr, "You won the game!\n");
+		}
+	}
 
-    // Show rendered frame
-    glutSwapBuffers();
+	// Draw circle
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(position.x, position.y);
 
-    // Display fps in window title.
-    if (frametime >= 1000)
-    {
-        char window_title[128];
-        snprintf(window_title, 128,
-                "Box2D: %f fps, level %d/%d",
-                frame_count / (frametime / 1000.f), current_level, num_levels);
-        glutSetWindowTitle(window_title);
-        last_time = time;
-        frame_count = 0;
-    }
+	for (int i = 0; i <= 360; i += 5) {
+		glVertex2f(position.x + sin(i) * ball_radius, position.y + cos(i) * ball_radius);
+	}
+
+	glEnd();
+
+	// Draw finish
+	point_t finish = levels[current_level].end;
+
+	glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_POLYGON);
+		glVertex2f(finish.x - finish_width, finish.y - finish_height);
+		glVertex2f(finish.x - finish_width, finish.y + finish_height);
+		glVertex2f(finish.x + finish_width, finish.y + finish_height);
+		glVertex2f(finish.x + finish_width, finish.y - finish_height);
+	glEnd();
+
+
+	// Show rendered frame
+	glutSwapBuffers();
+
+	// Display fps in window title.
+	if (frametime >= 1000) {
+		char window_title[128];
+		snprintf(window_title, 128,
+			"Box2D: %f fps, level %d/%d",
+			frame_count / (frametime / 1000.f), current_level, num_levels);
+		glutSetWindowTitle(window_title);
+		last_time = time;
+		frame_count = 0;
+	}
 }
 
 /*
  * Called when window is resized. We inform OpenGL about this, and save this
  * for future reference.
  */
-void resize_window(int width, int height)
-{
-    glViewport(0, 0, width, height);
-    reso_x = width;
-    reso_y = height;
+void resize_window(int width, int height) {
+	glViewport(0, 0, width, height);
+	reso_x = width;
+	reso_y = height;
 }
 
 /*
  * Called when the user presses a key.
  */
-void key_pressed(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-        case 27: // Esc
-        case 'q':
-            exit(0);
-            break;
-        // Add any keys you want to use, either for debugging or gameplay.
-        case ' ':
-            if (play) {
-                play = 0;
-            }
-            else {
-                play = 1;
-            }
-            break;
-        case '1':
-            load_world(0);
-            break;
-        case '2':
-            load_world(1);
-            break;
-        case '3':
-            load_world(2);
-            break;
-        case '4':
-            load_world(3);
-            break;
-        case '5':
-            load_world(4);
-            break;
-        default:
-            break;
-    }
+void key_pressed(unsigned char key, int x, int y) {
+	switch (key) {
+		case 27: // Esc
+		case 'q':
+			exit(0);
+			break;
+		// Add any keys you want to use, either for debugging or gameplay.
+		case ' ':
+			play = !play;
+			break;
+		case '1':
+			load_world(0);
+			break;
+		case '2':
+			load_world(1);
+			break;
+		case '3':
+			load_world(2);
+			break;
+		case '4':
+			load_world(3);
+			break;
+		case '5':
+			load_world(4);
+			break;
+		default:
+			break;
+	}
 }
 
 /*
  * Called when the user clicked (or released) a mouse buttons inside the window.
  */
-void mouse_clicked(int button, int state, int x, int y)
-{
+void mouse_clicked(int button, int state, int x, int y) {
 
 }
 
 /*
  * Called when the mouse is moved to a certain given position.
  */
-void mouse_moved(int x, int y)
-{
+void mouse_moved(int x, int y) {
 
 }
 
 
-int main(int argc, char **argv)
-{
-    // Create an OpenGL context and a GLUT window.
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(reso_x, reso_y);
-    glutCreateWindow("Box2D");
+int main(int argc, char **argv) {
+	// Create an OpenGL context and a GLUT window.
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(reso_x, reso_y);
+	glutCreateWindow("Box2D");
 
-    // Bind all GLUT events do callback function.
-    glutDisplayFunc(&draw);
-    glutIdleFunc(&draw);
-    glutReshapeFunc(&resize_window);
-    glutKeyboardFunc(&key_pressed);
-    glutMouseFunc(&mouse_clicked);
-    glutMotionFunc(&mouse_moved);
-    glutPassiveMotionFunc(&mouse_moved);
+	// Bind all GLUT events do callback function.
+	glutDisplayFunc(&draw);
+	glutIdleFunc(&draw);
+	glutReshapeFunc(&resize_window);
+	glutKeyboardFunc(&key_pressed);
+	glutMouseFunc(&mouse_clicked);
+	glutMotionFunc(&mouse_moved);
+	glutPassiveMotionFunc(&mouse_moved);
 
-    // Initialise the matrices so we have an orthogonal world with the same size
-    // as the levels, and no other transformations.
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, world_x, 0, world_y, 0, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	// Initialise the matrices so we have an orthogonal world with the same size
+	// as the levels, and no other transformations.
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, world_x, 0, world_y, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-    // Read the levels into a bunch of structs.
-    num_levels = load_levels(&levels);
-    printf("Loaded %d levels.\n", num_levels);
+	// Read the levels into a bunch of structs.
+	num_levels = load_levels(&levels);
+	printf("Loaded %d levels.\n", num_levels);
 
-    // Load the first level (i.e. create all Box2D stuff).
-    load_world(0);
+	// Load the first level (i.e. create all Box2D stuff).
+	load_world(0);
 
-    last_time = glutGet(GLUT_ELAPSED_TIME);
-    frame_count = 0;
-    glutMainLoop();
+	last_time = glutGet(GLUT_ELAPSED_TIME);
+	frame_count = 0;
+	glutMainLoop();
 
-    return 0;
+	return 0;
 }
